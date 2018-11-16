a5aabba0931ef9a716bca1d958451b2e99f56775
MHadley98 <43067353+MHadley98@users.noreply.github.com>
2018-11-16 15:32:44 +0000

    Added Level 2

---
+++ Assets/Script/Door.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Extra using statment to allow us to use the scene management functions
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+    //designer variable 
+    public string sceneToLoad;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //check if the thing we collideded with has player script
+        Player playerScript = collision.collider.GetComponent<Player>();
+
+        //only do something if the player runs into
+        if (playerScript != null)
+        {
+            //we did hit player
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
