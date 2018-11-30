fa148acc8a6f5a82a5987a683fcc18b3bcf1c538
MHadley98 <43067353+MHadley98@users.noreply.github.com>
2018-11-30 14:28:25 +0000

    Added Main Menu and Game Over Menu

---
+++ Assets/Script/StartButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Extra using statment to allow us to use the scene management functions
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+    //This will be called by the button component when it is clicked
+    public void StartGame()
+    {
+
+        //reset the score
+        PlayerPrefs.DeleteKey("score");
+
+        //Loads Level 1
+        SceneManager.LoadScene("Level1");
+
+    }
+
+}
---
+++ Assets/Script/TitleButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Extra using statment to allow us to use the scene management functions
+using UnityEngine.SceneManagement;
+
+
+public class TitleButton : MonoBehaviour {
+
+    //called when menu button is pressed
+    public void BackToMenu()
+    {
+        //return to menu scene
+        SceneManager.LoadScene("Title");
+
+    }
+
+}
